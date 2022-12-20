adf2a44d702b0c21e4ecb17d8dad1701e29cfc51
Jacob Botuck <jbotuck@gmail.com>
2022-12-19 19:43:59 -0600

    day 16 part 1

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,33 +1,54 @@
-import Action.OPEN
-import Action.TRAVEL_TO
 import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
     val valves = readInput("Day16")
         .map { Valve.of(it) }
         .associateBy { it.name }
-    val solutions = ArrayDeque<Solution>().apply {
-        addLast(Solution(listOf(TRAVEL_TO to valves["AA"]!!)))
-    }
+    val valuableValves = valves.values.filter { it.flowRate > 0 }.toSet()
+    val costs = mutableMapOf<Pair<String, String>, Int>()
+    val solutions = ArrayDeque<Solution>().apply { addLast(Solution(listOf("AA"))) }
     var maxRelief = 0
     while (solutions.isNotEmpty()) {
         val solution = solutions.removeFirst()
-        if (solution.isComplete()) {
-            maxRelief = max(maxRelief, solution.totalRelief())
+        var candidateDestinations = valuableValves.filter { it.name !in solution.visited }
+        for (destination in candidateDestinations) {
+            costs.computeIfAbsent(solution.lastVisited() to destination.name) {
+                valves.calculateCost(
+                    it.first,
+                    it.second
+                )
+            }
+        }
+        candidateDestinations =
+            candidateDestinations.filter { costs[solution.lastVisited() to it.name]!! < solution.timeRemaining() }
+        if (candidateDestinations.isEmpty()) {
+            maxRelief = max(maxRelief, solution.totalRelief(costs, valves))
             continue
         }
-        val lastAction = solution.actions.last()
-        if (lastAction.first == TRAVEL_TO && lastAction.second.flowRate > 0 && OPEN to lastAction.second !in solution.actions) {
-            solutions.add(solution.withAction(OPEN to lastAction.second))
-        }
-        val pointlessDestination = if (lastAction.first == TRAVEL_TO) {
-            solution.actions.run { getOrNull(lastIndex.dec())?.second?.name }
-        } else null
-        lastAction.second.neighbors.filter { it != pointlessDestination }.mapNotNull { valves[it] }
-            .map { solution.withAction(TRAVEL_TO to it) }
-            .let { solutions.addAll(it) }
+        solutions.addAll(
+            candidateDestinations.map { solution.with(it.name, costs[solution.lastVisited() to it.name]!!) }
+        )
     }
     println(maxRelief)
+}
+
+fun Map<String, Valve>.calculateCost(origin: String, destination: String): Int {
+    val costs = mutableMapOf<String, Int>().apply { set(origin, 0) }
+    val toVisit = mutableListOf(origin)
+    val visited = mutableSetOf<String>()
+    while (toVisit.isNotEmpty()) {
+        val visiting = toVisit.removeAt(toVisit.withIndex().minBy { costs[it.value] ?: Int.MAX_VALUE }.index)
+        if (visiting in visited) continue
+        visited.add(visiting)
+        if (visiting == destination) return costs[visiting]!!.inc() //Plus 1 to open the valve
+        val distanceOfNeighbor = costs[visiting]!!.inc()
+        for (neighbor in get(visiting)!!.neighbors) {
+            toVisit.add(neighbor)
+            costs[neighbor] = costs[neighbor]?.let { min(it, distanceOfNeighbor) } ?: distanceOfNeighbor
+        }
+    }
+    throw IllegalStateException("oops")
 }
 
 class Valve(val name: String, val flowRate: Int, val neighbors: List<String>) {
@@ -47,17 +68,17 @@ class Valve(val name: String, val flowRate: Int, val neighbors: List<String>) {
     }
 }
 
-enum class Action {
-    OPEN,
-    TRAVEL_TO
-}
-
-class Solution(val actions: List<Pair<Action, Valve>>) {
-    fun isComplete() = actions.size == 31
-    fun totalRelief() = actions
-        .withIndex()
-        .filter { it.value.first == OPEN }
-        .sumOf { it.value.second.totalRelief(30 - it.index) }
-
-    fun withAction(action: Pair<Action, Valve>) = Solution(actions + action)
+data class Solution(val visited: List<String>, val costIncurred: Int = 0) {
+    fun lastVisited() = visited.last()
+    fun timeRemaining(timeAllowed: Int = 30) = timeAllowed - costIncurred
+    fun with(name: String, newCost: Int) = Solution(visited + name, costIncurred + newCost)
+    fun totalRelief(costs: Map<Pair<String, String>, Int>, valves: Map<String, Valve>, timeAllowed: Int = 30): Int {
+        var elapsedTime = 0
+        var ret = 0
+        for (visit in visited.windowed(2)) {
+            elapsedTime += costs[visit.first() to visit.last()]!!
+            ret += valves[visit.last()]!!.totalRelief(timeAllowed - elapsedTime)
+        }
+        return ret
+    }
 }
